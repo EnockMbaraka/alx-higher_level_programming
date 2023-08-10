c249999e4b033de0557a8f6f4745e3e0ff1d319a
EnockCity <enockmwanzia17@gmail.com>
2023-08-10 16:14:30 +0300

    lists.h

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,14 +4,14 @@
 
 int check_cycle(listint_t *list)
 {
-		listint_t *fast = list, *slow = list;
+	listint_t *fast = list, *slow = list;
 
-			while (fast && slow && fast->next->next)
-					{
-								slow = slow->next;
-										fast = fast->next->next;
-												if (fast == slow)
-																return 1;
-													}
-				return 0;
+	while (fast && slow && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
 }
