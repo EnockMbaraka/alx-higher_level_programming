13cfae6c1b6465e32241c62234a88899eb1e6869
EnockCity <enockmwanzia17@gmail.com>
2023-08-10 05:58:59 +0300

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 
 
 def fizzbuzz():
@@ -6,8 +6,8 @@ def fizzbuzz():
         fb = ""
         if (i % 3 == 0):
             fb = "Fizz"
-            if (i % 5 == 0):
-                fb += "Buzz"
-            elif i % 3 != 0 and i % 5 != 0:
-                fb = i
-                print("{}".format(fb), end=" ")
+        if (i % 5 == 0):
+            fb += "Buzz"
+        elif i % 3 != 0 and i % 5 != 0:
+            fb = i
+        print("{}".format(fb), end=" ")
