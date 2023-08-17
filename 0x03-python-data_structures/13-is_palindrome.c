22d03804e7988fd81bd7b31134036e2d87b675c7
EnockCity <enockmwanzia17@gmail.com>
2023-08-17 03:06:41 +0300

    0-square_matrix_simple.py

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,27 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "lists.h"
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *left = *head;
-	listint_t *right;
-	listint_t *ptr = *head;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (left == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (ptr->next)
-		ptr = ptr->next;
-	right = ptr;
-	while (left->next != right && left != right)
+
+	tmp = *head;
+	while (tmp)
 	{
-		if (left->n != right->n)
-			return (0);
-		left = left->next;
-		ptr = *head;
-		while (ptr->next != right)
-			ptr = ptr->next;
-		right = ptr;
+		size++;
+		tmp = tmp->next;
 	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
 	return (1);
 }
+
