66c28e7330d2953416e3a2d57e83e1742aec450d
EnockCity <enockmwanzia17@gmail.com>
2023-08-19 04:40:19 +0300

    100-weight_average.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 1,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
-    }
-    num = 0
-    for i in range(len(man_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-            if (i != (len(roman_string) - 1) and
-                    roman_dict[roman_string[i]] < romadict[roman_string[i + 1]]):
-                    num += roman_dict[roman_string[i]] * -1
-        else:
-                num += roman_dict[roman_string[i]]
-    return (num)
+    roman_numerals = {
+                    "I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000}
+    value = 0
+    prev_value = 0
+    total = 0
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    for numeral in roman_string[::-1]:
+        if numeral not in roman_numerals:
+            return 0
+        value = roman_numerals[numeral]
+        prev_char = roman_string[roman_string.index(numeral) - 1]
+        if roman_string.index(numeral) != 0:
+            if prev_char not in roman_numerals:
+                return 0
+            if value // 10 > roman_numerals[prev_char]:
+                return 0
+            if (total > 5 and total != 10) and value == 1:
+                return 0
+            if value >= prev_value:
+                total += value
+            else:
+                total -= value
+            prev_value = value
+    return total
