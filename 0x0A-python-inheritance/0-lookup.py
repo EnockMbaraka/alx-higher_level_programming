d86cce3093bb5060fa69cd6b7280af2d54122ff9
EnockCity <enockmwanzia17@gmail.com>
2023-09-12 14:49:40 +0300

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
-##!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+#!/usr/bin/python3
+"""Define object class"""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
+    """retun the list of the object"""
     return (dir(obj))
