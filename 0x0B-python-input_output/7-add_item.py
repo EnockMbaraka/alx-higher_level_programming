90bc8fdafd7cbb254ad5960e65f79757f81b6aae
EnockCity <enockmwanzia17@gmail.com>
2023-09-13 03:44:25 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-""" A module containing the load, add and save function """
-import json
-import os
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-mylist = []
-for i in sys.argv[1:]:
-    mylist.append(i)
-
-if not os.path.exists("add_item.json"):
-    with open("add_item.json", "w") as f:
-        json.dump(mylist, f)
-
-else:
-    with open("add_item.json", "r") as f:
-        existing = json.load(f)
-    with open("add_item.json", "w") as f:
-        existing.extend(mylist)
-        json.dump(existing, f)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
