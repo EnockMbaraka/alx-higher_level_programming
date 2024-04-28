1ce3e05280e257152139d16655784688fb5552f4
EnockCity <enockmwanzia17@gmail.com>
2024-04-28 18:55:27 +0300

    updated

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""script that list all state objects that contain the letter a"""
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
-from model_state import Base, State
-import sys
+"""Changes the name of the State object with id = 2 to
+New Mexico in the database hbtn_0e_6_usa.
+Usage: ./12-model_state_update_id_2.py <mysql username> /
+                                       <mysql password> /
+                                       <database name>
+"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]))
-    Base.metadata.create_all(engine)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""script that list all state objects that contain the letter"""
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
-from model_state import Base, State
-import sys
+"""Deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa.
+Usage: ./13-model_state_delete_a.py <mysql username> /
+                                    <mysql password> /
+                                    <database name>
+"""
 
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).filter(State.name.like('%a%')):
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
         session.delete(state)
+
     session.commit()
