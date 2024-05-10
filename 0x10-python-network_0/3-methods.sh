616e5ea455c97cb192e5055ce4529b1583de120b
EnockCity <enockmwanzia17@gmail.com>
2024-05-10 04:58:33 +0300

    network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-## scrit that takes in a url and display all http methods the server will accept
-#curl -sI "$1" | grep "Allow" | cut -d " " -f2-
+# scrit that takes in a url and display all http methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
